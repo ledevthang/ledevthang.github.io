dcf5c6936730e64817c6b76ed3e89b282253ab44
thang <ledevthang@gmail.com>
2019-10-22 17:43:17 +0700

    add shit folder

---
+++ shit/_nuxt/8224994ce4c0b9c548af.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{235:function(n,t,e){"use strict";e.r(t);var c={components:{}},l=e(43),component=Object(l.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"index"},[this._v("\n  trang chu\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
